1cd54eb0aea854d912de129cdecfec1fa0cfe152
ajaysuperconstruct <ajay@superconstruct.io>
2025-06-04 22:23:37 +0530

    first commit

---
+++ Backend/app.js
@@ -0,0 +1,18 @@
+const express = require('express');
+const app = express();
+const dotenv = require('dotenv');
+const cors = require('cors');
+
+
+
+
+dotenv.config();
+app.use(cors());
+
+
+app.get('/', (req,res) =>{
+    res.send('hello world');
+})
+
+
+module.exports = app;
